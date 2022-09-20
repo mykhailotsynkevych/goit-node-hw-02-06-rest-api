84409d713397f7ebcf0fab9b01f5c58b80f44f45
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-09-20 12:50:49 +0300

    Start: +getListContacts,  + addContact

--- app.js
+++ app.js
@@ -3,14 +3,42 @@ const logger = require('morgan')
 const cors = require('cors')
 
 const contactsRouter = require('./routes/api/contacts')
+const contactsFunc = require('./models/contacts')
 
 const app = express()
+app.use(express.json())
+
+app.get('/api/contacts', async function (req, res) {
+  const contactsList = await contactsFunc.listContacts();
+  // console.table(contactsList);
+  res.send(contactsList);
+
+  // res.status(200).json(contactsList)
+})
+
+// app.get('/api/contacts/:id', async function (req, res) {
+//   const contactById = await contactsFunc.getContactById(id);
+//   // console.table(contactsList);
+//   res.send(contactById);
+
+//   // res.status(200).json(contactsList)
+// })
+
+app.post('/api/contacts', async function (req, res) {
+    const {name, email, phone} = req.body;
+    const newContact = await contactsFunc.addContact(name, email, phone);
+
+    res.send(newContact);
+
+    // res.status(201).json(newContact)
+})
+
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
 app.use(cors())
-app.use(express.json())
+
 
 app.use('/api/contacts', contactsRouter)
 
--- models/contacts.js
+++ models/contacts.js
@@ -1,12 +1,45 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, './contacts.json');
 
-const getContactById = async (contactId) => {}
+console.log(contactsPath);
+
+const listContacts = async () => {
+      const result = await fs.readFile(contactsPath);
+
+    return JSON.parse(result);
+}
+
+const getContactById = async (contactId) => {
+    //   const contactsList = await listContacts();
+    // const contactEl = contactsList.find(({id}) => id === contactId);
+
+    // if (!contactEl) {
+    //     throw new Error('Contact not found');
+    // }
+
+    // return contactEl;
+}
 
 const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+const addContact = async (name, email, phone) => {
+      const contactsList = await listContacts();
+    const  newContactEl = {
+        id: uuidv4(),
+        name, 
+        email,
+        phone
+    }
+
+    contactsList.push( newContactEl);
+
+    fs.writeFile(contactsPath, JSON.stringify(contactsList, null, 2));
+
+    return newContactEl;
+}
 
 const updateContact = async (contactId, body) => {}
 
