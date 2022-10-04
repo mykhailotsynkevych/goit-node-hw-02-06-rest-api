780eb4c27a5b6023ad34bb7e5158792298861a32
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-10-04 15:09:37 +0300

    Lekt. 6: Almost step 4 finish (+contactModel)

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-const mongoose = require('mongoose');
-// const { connect } = require("mongoose");
+const mongoose = require("mongoose");
 const { mongoURL } = require("./config");
 
-// const mongoURL =
-//   "mongodb+srv://tsynkevych:987654321@cluster0.xqzeagt.mongodb.net/?retryWrites=true&w=majority";
-// const { mongoURL } = require("./config");
 mongoose
   .connect(mongoURL, { dbName: "03-mongodb" })
   .then(() => console.log("Database connection successful"))
-  .catch((err) => console.log(err.message));
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
---
+++ services/index.js
@@ -0,0 +1,3 @@
+const Contact = require('../models/contactModel');
+
+// console.log(Contact)
