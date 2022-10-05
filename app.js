500a6c9cfbfe4ac87ec7dd03f5d4c6deb641b096
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-10-05 19:50:55 +0300

    Start: Step 1 finish

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const logger = require('morgan')
 const cors = require('cors')
 
 const contactsRouter = require('./routes/api/contacts')
+const usersRouter = require('./routes/api/users')
 
 const app = express()
 app.use(express.json())
@@ -12,7 +13,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 
-
+app.use('/api/users', usersRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
